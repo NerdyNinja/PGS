16209e27af25d3b402234da0117217743676c4b8
NerdyNinja <73026285+NerdyNinja@users.noreply.github.com>
2023-11-01 01:57:28 +0100

    BogusLib / UserInput twice entered no minuslife
    Add BogusLib and his word generator
    Added if Userinput was already enterd no more minuslife

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using Bogus;
 
 public class Global
 {
     public static string UserInput = "";
     public static char c;
+    public static char cInput;
     public static string Number;
     public static string[] List;
+    public static List<char> UsedChars = new List<char>();
     public static string Word;
     public static string Word1;
+    public static string RandomWord;
     public static int NumberWord;
     public static string UserWord;
     public static string UserWord1;
     public static int NumberEach = 0;
     public static int Lives = 2;
+    public static string GamemMode = "0";
     public static string charToString;
+    public static bool ContainsWhiteSpacesEg;
+    public static bool alreadyUsed = false;
     public static bool MinusLife = false;
     public static bool MinusLife1 = false;
     public static bool CorrectChar = false;
@@ -21,19 +32,27 @@ public class Global
 
 }
 
+    //TODO:
+    //Aktuellen Hangman zeichnen?
+    //Code Comments machen
+    //Je nach GameMode die Anzahl der Chars des generierenden Wort cappen
+    //
+
+    //Testing:
+    //Wörter werden random von einem web request geholt --> Wird jetzt durch BogusLib generiert, aktuell nur English (bug)?
+    //Schwierigkeitsstufen kann im UI gewählt werden --> Done? Sieht auf den ersten UseTest gut aus / muss debuggt werden
+    //Bereits Benutzte Buchstaben speichern und keine erneute Eingabe zulassen = kein MinusLeben mehr --> sollte Done sein
+
+
+      
+
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
         UI();
         StartGame();
-        GameLoop();
-        //TODO:
-        //Printen dass der User Minusleben bekommen hat
-        //Aktuellen Hangman zeichnen?
-        //Schwierigkeitsstufen kann im UI gewählt werden
-        //Wörter werden random von einem web request geholt
-        //
+        GameLoop();  
     }
 
     public static void UI()
@@ -60,6 +79,47 @@ public class HelloWorld
             if (Global.UserInput == "1")
             {
                 Global.Number = "1";
+                Console.WriteLine("Du willst also das Spiel starten, bist du dir sicher dass du der Herausforderung gewachsen bist?");
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+
+                do
+                {
+                Console.WriteLine("Welche Schwierigkeitsgrad moechtest du wählen?\nFolgendes steht zur Auswahl: Easy[1] -- Medium[2] -- Hard[3]");
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                Console.Write("Wäle deinen Schwierigskeitsgrad: ");
+
+                Global.GamemMode = Convert.ToString(Console.ReadLine());
+
+                    switch (Global.GamemMode)
+                    {
+                        case "1":
+                            Global.Lives = 6;
+                            Console.WriteLine($"Du hast dich für Easy entschieden, du besitzt also {Global.Lives} Leben!");
+                            Global.GamemMode = "1";
+                            break;
+
+                        case "2":
+                            Global.Lives = 4;
+                            Console.WriteLine($"Du hast dich für Easy entschieden, du besitzt also {Global.Lives} Leben!");
+                            Global.GamemMode = "2";
+                            break;
+                        
+                        case "3":
+                            Global.Lives = 2;
+                            Console.WriteLine($"Du hast dich für Easy entschieden, du besitzt also {Global.Lives} Leben!");
+                            Global.GamemMode = "3";
+                            break;
+                        
+                        default:
+                            Console.WriteLine("Das war keine korrekte Eingabe!");
+                            Global.GamemMode = "0";
+                            break;
+                    }
+
+                } while (Global.GamemMode == "0");
             }
 
             else if (Global.UserInput == "2")
@@ -80,27 +140,28 @@ public class HelloWorld
 
     public static void StartGame()
     {
+        var fakerWord = new Faker("de"); //Bug in Bogus? Erzeugt dennoch Englishe Wörter
+        
+        do 
+        {
+        var RandomWord = fakerWord.Random.Word();
 
-        Global.List = new string[6];
-        Global.List[0] = "Hallo";
-        Global.List[1] = "Kakadu";
-        Global.List[2] = "Atomkraftwerk";
-        Global.List[3] = "Menschlichkeit";
-        Global.List[4] = "Grünepolitik";
-        Global.List[5] = "Jeremyfegrance";
+        if (RandomWord.Contains('-') || RandomWord.Contains(' '))
+        {
+            Global.ContainsWhiteSpacesEg = true; 
+        }
 
-        Random rnd = new Random();
-        int index = rnd.Next(Global.List.Length);
-        Global.Word = Global.List[index];
+        Global.RandomWord = RandomWord.ToLower();
 
-        Global.Word1 = Global.Word.ToLower();
+        }while(Global.ContainsWhiteSpacesEg!=true);
 
-        foreach (char c in Global.Word)
+        
+        foreach (char c in Global.RandomWord)
         {
             Global.NumberWord++;
         }
 
-        foreach (char c in Global.Word)
+        foreach (char c in Global.RandomWord)
         {
             Global.UserWord += "#";
         }
@@ -120,21 +181,37 @@ public class HelloWorld
             Global.CorrectChar = false;
             Global.MinusLife = false;
 
-            Console.WriteLine(" ");
-            Console.Write("Gebe einen Buchstaben ein: ");
-            char cInput = Convert.ToChar(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(" ");
+                Console.Write("Gebe einen Buchstaben ein: ");
+
+                Global.cInput = Convert.ToChar(Console.ReadLine());
+                if (Global.UsedChars.Contains(Global.cInput))
+                {
+                    Console.WriteLine($"Den Buchstaben {Global.cInput} hast du bereits benutzt");
+                    Global.alreadyUsed = true;
+                }
+                else if (!Global.UsedChars.Contains(Global.cInput))
+                {
+                    Global.alreadyUsed = false;
+                }
+
+            }while(Global.alreadyUsed == true);
+
+            Global.UsedChars.Add(Global.cInput);
 
             for (int i = 0; i != Global.NumberWord; i++)
 
             {
                 if (UserWord[i] != '#')
                 {
-                    UserWord[i] = Global.Word1[i];
+                    UserWord[i] = Global.RandomWord[i];
                 }
 
-                else if (cInput == Global.Word1[i])
+                else if (Global.cInput == Global.RandomWord[i])
                 {
-                    UserWord[i] = Global.Word1[i];
+                    UserWord[i] = Global.RandomWord[i];
                     Global.CorrectChar = true;
                 }
 
@@ -148,30 +225,46 @@ public class HelloWorld
             {
                 Global.MinusLife = true;
             }
+            else
+            {
+                //Userword gab falschen Wert zrk daher der workaround aus line190
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                Console.WriteLine("Deine Eingabe war korrekt!");
+                Console.Write("Das aktuelle Wort lautet: ");
+            }
 
             if (Global.MinusLife == true)
             {
                 Global.Lives--;
-            }
 
-            if (Global.Lives == 0)
-            {
+                if (Global.Lives == 0)
+                {
                 Console.WriteLine("Game Over");
                 Console.WriteLine("###########");
                 Console.WriteLine("Du hast verloren");
+                Console.WriteLine(" ");
+                Console.WriteLine($"Das gesuchte Wort lautete {Global.RandomWord}!");
                 Console.ReadLine();
                 Environment.Exit(0);
+                }
+
+                Console.WriteLine("Du hast 1 Leben verloren!");
+                Console.WriteLine("Du besitzt noch: " + 
+                Global.Lives + " uebrige Leben!");
             }
 
+            //sollte das nicht in den else Block zu das aktuelle WOrt lautet..
             Console.WriteLine(UserWord);
 
             Global.charToString = new string(UserWord);
 
-            if (Global.charToString == Global.Word1)
+            if (Global.charToString == Global.RandomWord)
             {
-                Console.WriteLine("Du hast das Wort erraten! Es lautet: " + Global.Word);
+                Console.WriteLine("Du hast das Wort erraten! Es lautet: " + Global.RandomWord);
                 Console.WriteLine("##############");
                 Console.WriteLine("Glückwunsch!");
+                Console.ReadKey();
                 x = 0;
             }
         } while (x != 0);
